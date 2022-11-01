3a51edf968f044c6bc7d23c6bf56580a4f5c4711
wtucker <wtbtucker@gmail.com>
2022-11-01 14:05:32 -0400

    Working single timer with trampled

--- timer.js
+++ timer.js
@@ -1,10 +1,9 @@
-let IntervId;
+let gauntlet_timer;
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#inactive').style.display = 'block';
     document.querySelector('#active').style.display = 'none';
     document.querySelector('#start').addEventListener("click", start_program);
-    document.querySelector('#reset').addEventListener("click", reset_program);
 })
 
 function start_program() {
@@ -12,13 +11,44 @@ function start_program() {
     document.querySelector('#active').style.display = 'block';
     document.querySelector('#range_container').style.display = 'block';
     document.querySelector('#mage_container').style.display = 'none';
-    let timer = new Timer(change_view, 8000); // 1 second after first range attack, change on last range attack
+    gauntlet_timer = new timer(change_view, 12000);
+    // document.querySelector('#reset').addEventListener("click", gauntlet_timer.reset_program);
+    document.querySelector('#trampled').addEventListener("click", trampled);
 }
 
-// switch to a looped timer
-// add a conditional to Timer for finished state
-// Timer(change_view, 12000)
-// if trampled button, timer.add 3000
+// need an attribute to stop timer
+
+
+function timer(callback, time) {
+    let that = this,
+    execute = function () {
+        that.execute()
+    };
+    this.callback = callback;
+    this.extraTime = 0;
+    setTimeout(execute, time);
+}
+
+timer.prototype.execute = function () {
+    let that = this,
+    execute = function () {
+        that.execute()
+    };
+    if (this.extraTime) {
+        setTimeout(execute, this.extraTime);
+        this.extraTime = 0;
+    } else {
+        this.callback();
+    }
+};
+
+timer.prototype.addTime = function() {
+    this.extraTime += 3000;
+}
+
+function trampled() {
+    if (gauntlet_timer) {gauntlet_timer.addTime(3000);}
+}
 
 function change_view() {
     let range_view = document.querySelector('#range_container');
@@ -33,34 +63,9 @@ function change_view() {
 }
 
 function reset_program() {
-    clearInterval(IntervId);
-    IntervId = null;
-    document.querySelector('#inactive').style.display = 'block';
-    document.querySelector('#active').style.display = 'none';
-}
-
-function Timer(callback, time) {
-    this.setTimeout(callback, time);
-}
-
-Timer.prototype.setTimeout = function(callback, time) {
-    var self = this;
-    if(this.timer) {
-        clearTimeout(this.timer);
-    }
-    this.finished = false;
-    this.callback = callback;
-    this.time = time;
-    this.timer = setTimeout(function() {
-        self.finished = true;
-        callback();
-    }, time);
-    this.start = Date.now();
-}
-
-Timer.prototype.add = function(time) {
-    if(!this.finished) {
-        time = this.time - (Date.now() - this.start) + time;
-        this.setTimeout(this.callback, time);
-    }
-}
+    if (tim) {
+        tim.running = false;
+        document.querySelector('#inactive').style.display = 'block';
+        document.querySelector('#active').style.display = 'none';
+    };
+};
